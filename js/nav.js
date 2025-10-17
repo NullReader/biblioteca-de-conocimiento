6a6eed1b593dfe57224cd482b556357a1febe607
nullreader <nullreader1@gmail.com>
2025-10-17 17:03:18 -0300

    responsive fix nav footer

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,7 @@
-/* Menú Hamburguesa ------------------------- */
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navLinks.classList.toggle("active");
-});
-
-/* Buscador Responsive -------------------- */
-const searchToggle = document.getElementById("searchToggle");
-const searchField = document.querySelector(".search-field");
-
-searchToggle.addEventListener("click", () => {
-    searchField.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
 });
